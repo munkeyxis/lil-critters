78b395dae3f4f6750052f64c2637702e9bb87168
Chris Stevens <munkeyxis@gmail.com>
2017-04-11 23:06:44 -0400

    reduce energy when moving

--- Assets/Scripts/CritterMovement.cs
+++ Assets/Scripts/CritterMovement.cs
@@ -4,14 +4,26 @@ public class CritterMovement : MonoBehaviour
 {
     public float movementSpeed;
     public float rotationSpeed;
+    private Metabolism metabolism;
+    private float moveForwardEnergyCost;
+    private float turnEnergyCost;
+
+    private void Start()
+    {
+        metabolism = GetComponent<Metabolism>();
+        moveForwardEnergyCost = .025f;
+        turnEnergyCost = .01f;
+    }
 
     public void Forward()
     {
+        metabolism.LoseEnergy(moveForwardEnergyCost);
         transform.localPosition += transform.forward * Time.deltaTime * movementSpeed;
     }
 
     public void TurnClockwise()
     {
+        metabolism.LoseEnergy(turnEnergyCost);
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
     }
 }
