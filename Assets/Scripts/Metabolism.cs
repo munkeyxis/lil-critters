605e2a9e5442ffa31e28b37ef1d95b192ed3573e
Chris Stevens <munkeyxis@gmail.com>
2017-04-11 22:13:10 -0400

    create basic Metabolism

---
+++ Assets/Scripts/Metabolism.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Metabolism : MonoBehaviour 
+{
+    private float energyQuantity;
+    private float baseMetabolicRate;
+
+    private void Start() 
+    {
+        // TODO: initial eneryQuantity should be set by genetics.
+        energyQuantity = 50;
+        baseMetabolicRate = 1;
+    }
+
+    private void Update()
+    {
+        energyQuantity -= baseMetabolicRate * Time.deltaTime;
+    }
+}
